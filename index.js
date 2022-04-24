5b1a9253eb18ae10a166563b17d7a6cb8296f6f9
Soumyajit <soumyajitd2000@gmail.com>
2022-04-24 17:10:32 +0530

    file upload api and other changes

--- index.js
+++ index.js
@@ -16,13 +16,24 @@ mongoose
   .then(console.log('connected to mongoose'))
   .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images")
+  }, filename: (req, file, cb)=>{
+    cb(null, req.body.name)
+  }
+})
+
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"), (req, res)=>{
+  res.status(200).json("File Uploaded");
+})
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute)
 
-app.use("/roy", (req, res) => {
-  console.log("Hello")
-  console.log("everyone ,  welcome to my  college project")
-})
 app.listen(7000, () => {
 
   console.log("Backend is running");
